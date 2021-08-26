b43761b36136095f0f174d4eeae3351e23afca72
EmilyDSarani <emily.sarani@gmail.com>
2021-08-26 18:27:19 -0500

    alert on button. Click for a...sweet...surprise

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 // import functions and grab DOM elements
-
+const button = document.getElementById('button');
 // initialize global state
+button.addEventListener('click', () =>{
+    alert('Have you heard about the chocolate box thief? He’s always got a few Twix up his sleeve.');
 
+});
 // set event listeners 
   // get user input
   // use user input to update state 
